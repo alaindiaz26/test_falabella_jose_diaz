946ae4cecb90828edb261408d275b3ef61c1cdba
Alain Diaz <jadcve@gmail.com>
2020-06-26 13:45:16 -0400

    cambios de archivos

--- main.php
+++ main.php
@@ -5,30 +5,31 @@
  * Date: 26/06/2020
  */
 
-require_once 'test.php';
-require_once 'falabella_test.php';
- 
-/* 
- *  Main function
- */ 
+ function main() {
+      $result ='';
+        for ( $i=1;$i<=100;$i++) 
+        {
+            $valueToCheck = $i;
+            $valueToCheck = getDiv($valueToCheck,5,3,"Linio");
+            $valueToCheck = getDiv($valueToCheck,3,3,"Falabella");
+            $valueToCheck = getDiv($valueToCheck,5,5,"IT");
+            $result .= $valueToCheck.",";
+        }
+      return $result;
+} 
 
-echo '<h2>'. main()."<br /> <br /> </h2>";
-
-
-/*
-* Testing 
-*/
-
-//var_dump(test());
-
-switch(test())
-{
-    case true:
-        echo '<h2>'.'Passed Successfully!!!'.'</h2>';
-    break;
-
-    case false:
-        echo '<h2>'.'Oops something went wrong!!!'.'</h2>';
-    break;
+function getDiv($value,$firstValue,$secondValue,$returnWord) {
+  
+        if (is_numeric($value) && ($value % $firstValue==0) && ($value % $secondValue==0))
+        {
+          return $returnWord;
+        } 
+        return $value;
 }
+
+
+ 
+
+
+
 
